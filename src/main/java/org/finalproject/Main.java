e24e427ec1b7b26c4413481d24986c7672dae19e
leradent1989 <leranmu@gmail.com>
2023-06-18 15:43:11 +0300

    test commit

--- src/main/java/org/finalproject/Main.java
+++ src/main/java/org/finalproject/Main.java
@@ -2,6 +2,6 @@ package org.finalproject;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world!1");
     }
 }
