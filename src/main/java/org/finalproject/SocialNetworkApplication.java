b58bc542f26a548bba377f4d84d0dc7bcf2558b3
leradent1989 <leranmu@gmail.com>
2023-06-21 18:47:14 +0300

    test commit

--- src/main/java/org/finalproject/SocialNetworkApplication.java
+++ src/main/java/org/finalproject/SocialNetworkApplication.java
@@ -14,6 +14,7 @@ public class SocialNetworkApplication implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
         System.out.println("http://localhost:9000/swagger-ui/index.html \n");
+
         System.out.println("http://localhost:9000/h2-console");
     }
 }
