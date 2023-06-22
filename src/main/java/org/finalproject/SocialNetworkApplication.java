60675a829eae179e865f60dfd8f7777e53203955
Tetiana Pysariuha <tetyanka.p.tv@gmail.com>
2023-06-22 21:47:13 +0300

    feat: set SonarCloud and Coveralls

--- src/main/java/org/finalproject/SocialNetworkApplication.java
+++ src/main/java/org/finalproject/SocialNetworkApplication.java
@@ -1,9 +1,14 @@
 package org.finalproject;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SocialNetworkApplication implements ApplicationRunner {
@@ -13,7 +18,7 @@ public class SocialNetworkApplication implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println("http://localhost:9000/swagger-ui/index.html \n");
         System.out.println("http://localhost:9000/h2-console");
     }
+
 }
