9371adc45ba8062857dadbc02295e7d3978e2439
romanlitvinenko <umot12@gmail.com>
2023-06-22 20:57:52 +0300

    add configureSwagger

--- src/main/java/org/finalproject/SocialNetworkApplication.java
+++ src/main/java/org/finalproject/SocialNetworkApplication.java
@@ -1,9 +1,14 @@
 package org.finalproject;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SocialNetworkApplication implements ApplicationRunner {
@@ -15,5 +20,16 @@ public class SocialNetworkApplication implements ApplicationRunner {
     public void run(ApplicationArguments args) throws Exception {
         System.out.println("http://localhost:9000/swagger-ui/index.html \n");
         System.out.println("http://localhost:9000/h2-console");
+    }
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("EIS API")
+                        .description("Employee Information System sample application")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org"))
+                        .description("SpringShop Wiki Documentation")
+                        .contact(new Contact().email("test@test.com").url("http://fullstackcode.dev")))
+                ;
     }
 }
