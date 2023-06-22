10a12196ba6618713926c68dadbc205470fb4745
leradent1989 <leranmu@gmail.com>
2023-06-22 19:32:47 +0300

    feat:add passwordEncoder bean

--- src/main/java/org/finalproject/SocialNetworkApplication.java
+++ src/main/java/org/finalproject/SocialNetworkApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class SocialNetworkApplication implements ApplicationRunner {
@@ -16,4 +19,10 @@ public class SocialNetworkApplication implements ApplicationRunner {
         System.out.println("http://localhost:9000/swagger-ui/index.html \n");
         System.out.println("http://localhost:9000/h2-console");
     }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+
+        return new BCryptPasswordEncoder();
+    }
+
 }
