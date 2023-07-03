5598c52e9dd6c3e601e582c16a889a0553e7fe68
TetianaPysariuha <108232607+TetianaPysariuha@users.noreply.github.com>
2023-07-03 22:41:39 +0300

    Update ChatRepository.java
    fixed: Checkstyle violations (comments)

--- src/main/java/org/finalproject/repository/ChatRepository.java
+++ src/main/java/org/finalproject/repository/ChatRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public interface ChatRepository extends RepositoryInterface<Chat>, JpaSpecificationExecutor<Chat> {
 
-    //need to be in UserRepo?
+//    need to be in UserRepo?
 //    @Query("Select id FROM Users m WHERE fullName LIKE %:partOfName% ")
 //    List<Long> findUserIdByPartOfName(String partOfName);
 
