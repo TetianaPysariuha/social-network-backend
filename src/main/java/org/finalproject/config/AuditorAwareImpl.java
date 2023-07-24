7af6b33586d95be7c5d57a804de76713d7c0b041
leradent1989 <leranmu@gmail.com>
2023-07-24 18:23:43 +0300

    fix: update AuditorAwareImpl

--- src/main/java/org/finalproject/config/AuditorAwareImpl.java
+++ src/main/java/org/finalproject/config/AuditorAwareImpl.java
@@ -2,6 +2,7 @@ package org.finalproject.config;
 
 import org.springframework.data.domain.AuditorAware;
 //import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -11,8 +12,8 @@ public class AuditorAwareImpl implements AuditorAware<String> {
 
     public Optional<String> getCurrentAuditor() {
         // until there is no authentification
-        return Optional.of("Alex Smith");
-//        return Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString());
+      //  return Optional.of("Alex Smith");
+       return Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString());
     }
 }
 
