d8cb418fba1f986fcf46b74aaff362d6585e1652
Максим Галушка <maksgalushka@gmail.com>
2023-08-12 15:38:24 +0300

    feat: add some change

--- src/main/java/org/finalproject/entity/Post.java
+++ src/main/java/org/finalproject/entity/Post.java
@@ -15,8 +15,6 @@ import java.util.Set;
 
 @AllArgsConstructor
 @NoArgsConstructor
-
-@EqualsAndHashCode(of = {"id", "user"})
 @Getter
 @Setter
 @Entity
@@ -25,7 +23,6 @@ import java.util.Set;
 public class Post extends BaseEntity{
     @ManyToOne
     @JoinColumn(name = "user_id")
-    @JsonIgnore
     private User user;
     @Column(name = "post_type")
     private String postType;
