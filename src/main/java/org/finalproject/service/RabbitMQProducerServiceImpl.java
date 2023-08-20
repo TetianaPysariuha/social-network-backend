0c7815e92f496739cdaf7e3449d94d925ec60042
leradent1989 <leranmu@gmail.com>
2023-08-20 14:38:01 +0300

    fix:new migration

--- src/main/java/org/finalproject/service/RabbitMQProducerServiceImpl.java
+++ src/main/java/org/finalproject/service/RabbitMQProducerServiceImpl.java
@@ -1,10 +1,12 @@
 package org.finalproject.service;
 
+import jakarta.transaction.Transactional;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Transactional
 public class RabbitMQProducerServiceImpl implements RabbitMQProducerService {
 
     private final RabbitTemplate rabbitTemplate;
