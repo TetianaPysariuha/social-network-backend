be8cf58a054d6fe7ee7491f1788d2d2cbae2d845
Valeria Vitvytska <leranmu@gmail.com>
2023-08-23 19:32:24 +0300

    Upload user images (#89)
    * feat:update JwtProvider
    
    * fix:remove checkstyle violation
    
    * fix:remove checkstyle violation
    
    * fix:remove checkstyle violation2
    
    * fix:return eqauls hashcode for post
    
    * fix:return eqauls hashcode for auth entities
    
    * fix:surround login with try catch
    
    * fix:remove checkstyle violations
    
    * fix:change jwt filter class
    
    * fix:new migration
    
    * fix:update SecurityConfig
    
    * fix:update AuthService
    
    * fix:update AuthService UserRestController remove unused files
    
    * fix:update AuthService
    
    * feat:uploadUserImages
    
    * feat:uploadUserImages
    
    * fix:json ignore password

--- src/main/java/org/finalproject/dto/UserDto.java
+++ src/main/java/org/finalproject/dto/UserDto.java
@@ -1,8 +1,10 @@
 package org.finalproject.dto;
 
 import lombok.*;
+import org.finalproject.entity.UserImage;
 
 import java.util.Date;
+import java.util.List;
 
 @Getter
 @Setter
@@ -32,5 +34,7 @@ public class UserDto {
     private String about;
 
     private String profileBackgroundPicture;
+
+    private List<UserImage> userImages;
 
 }
