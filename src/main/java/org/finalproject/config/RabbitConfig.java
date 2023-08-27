0718553b54875e50e02f5bca4bf5d93b0255107d
V1nitar <136800162+V1nitar@users.noreply.github.com>
2023-08-28 00:12:32 +0300

    Update RabbitConfig.java (#97)

--- src/main/java/org/finalproject/config/RabbitConfig.java
+++ src/main/java/org/finalproject/config/RabbitConfig.java
@@ -23,14 +23,20 @@ public class RabbitConfig {
     @Value("${cloudamqp_url:localhost}")
     private String cloudamqpUrl;
 
-    @Bean
-    public ConnectionFactory connectionFactory() {
+    // @Bean
+    // public ConnectionFactory connectionFactory() {
 
-        CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory(cloudamqpUrl);
-        // cachingConnectionFactory.setUsername(rabbitUserAndVHost);
-        // cachingConnectionFactory.setPassword(rabbitPass);
-        // cachingConnectionFactory.setVirtualHost(rabbitUserAndVHost);
-        return cachingConnectionFactory;
+    //     CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory(cloudamqpUrl);
+    //     // cachingConnectionFactory.setUsername(rabbitUserAndVHost);
+    //     // cachingConnectionFactory.setPassword(rabbitPass);
+    //     // cachingConnectionFactory.setVirtualHost(rabbitUserAndVHost);
+    //     return cachingConnectionFactory;
+    // }
+    @Bean
+    public CachingConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+        connectionFactory.setUri("amqps://tnjaskux:CwZKUs5tzAcTPPlLIhT9vLxTWH7km98o@whale.rmq.cloudamqp.com/tnjaskux");
+        return connectionFactory;
     }
 
     @Bean
