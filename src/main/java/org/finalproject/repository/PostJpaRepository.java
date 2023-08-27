14ca03533c4980f63b6ce0d18fa90d076c739017
MaximGalushka <101721081+MaximGalushka@users.noreply.github.com>
2023-08-27 23:47:04 +0300

    Fix comments (#95)
    * feat: change postRestController commentPost endpoint and add logger
    
    * fix: change repo methods findAllPosts and findAllPosts(pageble)
    
    * fix: add new repo methods findAllPosts
    
    * feat: add dto on posts
    
    * fix: userController fix
    
    * feat: add creating Post functional
    
    * feat: add creating Post functional1

--- src/main/java/org/finalproject/repository/PostJpaRepository.java
+++ src/main/java/org/finalproject/repository/PostJpaRepository.java
@@ -1,7 +1,17 @@
 package org.finalproject.repository;
 
 import org.finalproject.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostJpaRepository extends RepositoryInterface<Post> {
+    @Query("SELECT p FROM Post p WHERE p.postType = 'post'")
+    List<Post> findAllPosts();
+
+    @Query("SELECT p FROM Post p WHERE p.postType = 'post'")
+    Page<Post> findAllPosts(Pageable pageable);
 
 }
--- src/main/java/org/finalproject/service/DefaultPostService.java
+++ src/main/java/org/finalproject/service/DefaultPostService.java
@@ -4,11 +4,24 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.finalproject.entity.Post;
 import org.finalproject.repository.PostJpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class DefaultPostService extends GeneralService<Post> {
-    private final PostJpaRepository userRepository;
+    private final PostJpaRepository postRepository;
+
+    public List<Post> findAllPosts() {
+        return postRepository.findAllPosts();
+    }
+
+    public Page<Post> findAllPosts(Pageable pageable) {
+        return postRepository.findAllPosts(pageable);
+    }
+
 }
