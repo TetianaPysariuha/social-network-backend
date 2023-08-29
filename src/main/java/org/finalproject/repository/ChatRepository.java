c27f8242eba3638008c0e9b38efb02b66167ae60
Aleksandr <aleks.kolesnyk.kt@gmail.com>
2023-08-29 16:26:15 +0300

    .

--- src/main/java/org/finalproject/repository/ChatRepository.java
+++ src/main/java/org/finalproject/repository/ChatRepository.java
@@ -10,6 +10,30 @@ import java.util.List;
 
 public interface ChatRepository extends RepositoryInterface<Chat>, JpaSpecificationExecutor<Chat> {
 
+    //    @Query(value = "SELECT DISTINCT " +
+//            "c.id AS id, " +
+//            "uc.user_id AS userId, " +
+//            "u.full_name AS fullName, " +
+//            "u.profile_picture AS profilePicture, " +
+//            "m.content AS content, " +
+//            "m.created_date AS lastMessageDate " +
+//            "FROM " +
+//            "chats c " +
+//            "JOIN " +
+//            "users_chats uc ON c.id = uc.chat_id " +
+//            "JOIN " +
+//            "users u ON uc.user_id = u.id " +
+//            "LEFT JOIN " +
+//            "messages m ON c.id = m.chat_id " +
+//            "WHERE " +
+//            "u.id <> :id " +
+//            "AND c.id IN (SELECT cc.chat_id FROM users_chats cc WHERE cc.user_id = :id) " +
+//            "AND m.id IS NULL OR m.id  = ( " +
+//            "SELECT MAX(m2.id) " +
+//            "FROM messages m2 " +
+//            "WHERE m2.chat_id = c.id " +
+//            ")", nativeQuery = true)
+//    List<ChatSpecProjection> getChatsForUserExceptUserId(@Param("id") Long userId);
     @Query(value = "SELECT DISTINCT " +
             "c.id AS id, " +
             "uc.user_id AS userId, " +
@@ -28,7 +52,7 @@ public interface ChatRepository extends RepositoryInterface<Chat>, JpaSpecificat
             "WHERE " +
             "u.id <> :id " +
             "AND c.id IN (SELECT cc.chat_id FROM users_chats cc WHERE cc.user_id = :id) " +
-            "AND m.id IS NULL OR m.id  = ( " +
+            "AND m.id = ( " +
             "SELECT MAX(m2.id) " +
             "FROM messages m2 " +
             "WHERE m2.chat_id = c.id " +
