3d77b6a5b764d501efaf3dd309e066da6e7a799a
leradent1989 <leranmu@gmail.com>
2023-09-16 22:57:10 +0300

    feat:comments for user images

---
+++ src/main/java/org/finalproject/dto/ImgCommentDtoMapper.java
@@ -0,0 +1,41 @@
+package org.finalproject.dto;
+
+import org.finalproject.entity.ImgComment;
+import org.finalproject.facade.GeneralFacade;
+import org.finalproject.service.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ImgCommentDtoMapper extends GeneralFacade<ImgComment, ImgCommentDtoRequest, ImgCommentDto> {
+    @Autowired
+    DefaultUserImageService imageService;
+
+    @Autowired
+    DefaultUserService userService;
+
+
+    @Autowired
+    private UserDtoMapper dtoMapper;
+
+    @Autowired
+    private UserImageDtoMapper imageMapper;
+
+
+
+    @Override
+    public void decorateDto(ImgCommentDto dto, ImgComment entity) {
+
+        dto.setAuthor(dtoMapper.convertToDto(entity.getAuthor()));
+
+    }
+
+    @Override
+    public void decorateEntity(ImgComment entity, ImgCommentDtoRequest dto) {
+
+       entity.setAuthor(userService.getOne(dto.getAuthorId()));
+        entity.setImage(imageService.getOne(dto.getImageId()));
+
+    }
+}
+
