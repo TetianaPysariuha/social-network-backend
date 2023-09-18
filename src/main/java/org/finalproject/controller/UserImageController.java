6b44854016ba39dd3bfefbc16d24454603fd97bb
leradent1989 <leranmu@gmail.com>
2023-09-18 16:46:08 +0300

    fix: update user image controller

--- src/main/java/org/finalproject/controller/UserImageController.java
+++ src/main/java/org/finalproject/controller/UserImageController.java
@@ -2,15 +2,23 @@ package org.finalproject.controller;
 
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
-import org.finalproject.dto.UserImageDto;
-import org.finalproject.dto.UserImageDtoMapper;
-import org.finalproject.dto.UserImageDtoRequest;
+import org.finalproject.dto.*;
+import org.finalproject.entity.Notification;
+import org.finalproject.entity.User;
 import org.finalproject.entity.UserImage;
+import org.finalproject.service.DefaultUserImageService;
+import org.finalproject.service.DefaultUserService;
 import org.finalproject.service.GeneralService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 
@@ -23,6 +31,10 @@ public class UserImageController {
 
     private final UserImageDtoMapper dtoMapper;
 
+    private final DefaultUserService defaultUserService;
+
+    private final DefaultUserImageService defaultUserImageService;
+
     @GetMapping
     public List<UserImageDto> getAll() {
 
@@ -31,6 +43,38 @@ public class UserImageController {
                 .map(dtoMapper::convertToDto)
                 .collect(Collectors.toList());
 
+    }
+
+    @GetMapping("/{page}/{size}")
+
+    public ResponseEntity<List<UserImageDto>> findAll(@PathVariable Integer page, @PathVariable Integer size) {
+        Sort sort = Sort.by(new Sort.Order(Sort.Direction.ASC, "id"));
+        Pageable pageable = PageRequest.of(page, size, sort);
+        List<UserImageDto> userImageDtoList = userImageService.findAll(pageable).toList()
+                .stream()
+                .map(dtoMapper::convertToDto)
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(userImageDtoList);
+    }
+
+    @GetMapping("/user/{page}/{size}")
+    public ResponseEntity<?> getAuthorizedUserImagesPageable(@PathVariable Integer page, @PathVariable Integer size) {
+        String auth = SecurityContextHolder.getContext().getAuthentication().getName();
+        Optional<User> profile = defaultUserService.getByFullName(auth);
+        Sort sort = Sort.by(new Sort.Order(Sort.Direction.ASC, "id"));
+        Pageable pageable = PageRequest.of(page, size, sort);
+        if (profile.isEmpty()) {
+            throw new EntityNotFoundException();
+        }
+        Page<UserImage> images  = defaultUserImageService.findAuthUserImagesPageable(profile.get().getId(),pageable);
+        List<UserImageDto> userImageDto = images
+                .stream()
+                .map(dtoMapper::convertToDto)
+                .collect(Collectors.toList());
+
+
+        return ResponseEntity.ok().body(userImageDto);
     }
 
     @PutMapping
